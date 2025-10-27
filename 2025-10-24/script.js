a47180317171b6c5ef4a136f1eeb94e44da8ab6b
Tetri Mesquita Neto <tetri.neto@aliare.co>
2025-10-27 09:25:37 -0300

    refactor: remove commented-out code in dive function

--- 2025-10-24/script.js
+++ 2025-10-24/script.js
@@ -1,11 +1,7 @@
 function dive(map, coordinates) {
   let collected = map[coordinates[0]][coordinates[1]];
 
-  //console.log(`Diving at [${coordinates[0]}, ${coordinates[1]}]: ${collected}`);
-
   if (collected == "-") return "Empty";
-
-  //if (collected == "X") return "Found";
 
   if (collected == "O" || collected == "X") {
     map[coordinates[0]][coordinates[1]] = "X";
