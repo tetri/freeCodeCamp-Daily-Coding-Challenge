0e0c126829b5d08ae1ee7db335bee6c4caa34ccf
Tetri Mesquita Neto <tetri.neto@aliare.co>
2025-11-11 13:35:41 -0300

    IPv4 Validator

---
+++ 2025/09/05/script.js
@@ -0,0 +1,24 @@
+function isValidIPv4(ipv4) {
+  let parts = ipv4.split(".");
+
+  if (parts.length != 4) return false;
+
+  console.log(parts);
+
+  for (let i = 0; i < parts.length; i++) if (!validPart(parts[i])) return false;
+
+  return true;
+}
+
+function validPart(part) {
+  if (part == "") return false;
+
+  let number = parseInt(part);
+  if (number > 255) return false;
+
+  if (part != number.toString()) return false;
+
+  return true;
+}
+
+module.exports = isValidIPv4;
---
+++ 2025/09/05/script.test.js
@@ -0,0 +1,37 @@
+const isValidIPv4 = require("./script");
+
+//1. isValidIPv4("192.168.1.1") should return true.
+//2. isValidIPv4("0.0.0.0") should return true.
+//3. isValidIPv4("255.01.50.111") should return false.
+//4. isValidIPv4("255.00.50.111") should return false.
+//5. isValidIPv4("256.101.50.115") should return false.
+//6. isValidIPv4("192.168.101.") should return false.
+//7. isValidIPv4("192168145213") should return false.
+
+test("isValidIPv4 1", () => {
+  expect(isValidIPv4("192.168.1.1")).toBe(true);
+});
+
+test("isValidIPv4 2", () => {
+  expect(isValidIPv4("0.0.0.0")).toBe(true);
+});
+
+test("isValidIPv4 3", () => {
+  expect(isValidIPv4("255.01.50.111")).toBe(false);
+});
+
+test("isValidIPv4 4", () => {
+  expect(isValidIPv4("255.00.50.111")).toBe(false);
+});
+
+test("isValidIPv4 5", () => {
+  expect(isValidIPv4("256.101.50.115")).toBe(false);
+});
+
+test("isValidIPv4 6", () => {
+  expect(isValidIPv4("192.168.101.")).toBe(false);
+});
+
+test("isValidIPv4 7", () => {
+  expect(isValidIPv4("192168145213")).toBe(false);
+});
