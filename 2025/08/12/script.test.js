0ec6a742705b04dd2bd72323ec900bfa78f410fb
Tetri Mesquita Neto <tetri.neto@aliare.co>
2025-11-13 17:57:41 -0300

    Adjust tests

--- 2025/08/12/script.test.js
+++ 2025/08/12/script.test.js
@@ -18,65 +18,65 @@ const isValidNumber = require("./script");
 //16. isValidNumber("z", 36) should return true.
 
 test("isValidNumber 1", () => {
-  expect(isValidNumber("10101", 2)).toBe(true);
+  expect(isValidNumber("10101", 2)).toStrictEqual(true);
 });
 
 test("isValidNumber 2", () => {
-  expect(isValidNumber("10201", 2)).toBe(false);
+  expect(isValidNumber("10201", 2)).toStrictEqual(false);
 });
 
 test("isValidNumber 3", () => {
-  expect(isValidNumber("76543210", 8)).toBe(true);
+  expect(isValidNumber("76543210", 8)).toStrictEqual(true);
 });
 
 test("isValidNumber 4", () => {
-  expect(isValidNumber("9876543210", 8)).toBe(false);
+  expect(isValidNumber("9876543210", 8)).toStrictEqual(false);
 });
 
 test("isValidNumber 5", () => {
-  expect(isValidNumber("9876543210", 10)).toBe(true);
+  expect(isValidNumber("9876543210", 10)).toStrictEqual(true);
 });
 
 test("isValidNumber 6", () => {
-  expect(isValidNumber("ABC", 10)).toBe(false);
+  expect(isValidNumber("ABC", 10)).toStrictEqual(false);
 });
 
 test("isValidNumber 7", () => {
-  expect(isValidNumber("ABC", 16)).toBe(true);
+  expect(isValidNumber("ABC", 16)).toStrictEqual(true);
 });
 
 test("isValidNumber 8", () => {
-  expect(isValidNumber("Z", 36)).toBe(true);
+  expect(isValidNumber("Z", 36)).toStrictEqual(true);
 });
 
 test("isValidNumber 9", () => {
-  expect(isValidNumber("ABC", 20)).toBe(true);
+  expect(isValidNumber("ABC", 20)).toStrictEqual(true);
 });
 
 test("isValidNumber 10", () => {
-  expect(isValidNumber("4B4BA9", 16)).toBe(true);
+  expect(isValidNumber("4B4BA9", 16)).toStrictEqual(true);
 });
 
 test("isValidNumber 11", () => {
-  expect(isValidNumber("5G3F8F", 16)).toBe(false);
+  expect(isValidNumber("5G3F8F", 16)).toStrictEqual(false);
 });
 
 test("isValidNumber 12", () => {
-  expect(isValidNumber("5G3F8F", 17)).toBe(true);
+  expect(isValidNumber("5G3F8F", 17)).toStrictEqual(true);
 });
 
 test("isValidNumber 13", () => {
-  expect(isValidNumber("abc", 10)).toBe(false);
+  expect(isValidNumber("abc", 10)).toStrictEqual(false);
 });
 
 test("isValidNumber 14", () => {
-  expect(isValidNumber("abc", 16)).toBe(true);
+  expect(isValidNumber("abc", 16)).toStrictEqual(true);
 });
 
 test("isValidNumber 15", () => {
-  expect(isValidNumber("AbC", 16)).toBe(true);
+  expect(isValidNumber("AbC", 16)).toStrictEqual(true);
 });
 
 test("isValidNumber 16", () => {
-  expect(isValidNumber("z", 36)).toBe(true);
+  expect(isValidNumber("z", 36)).toStrictEqual(true);
 });
