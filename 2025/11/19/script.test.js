4ed65d4afaf01a7b752e74a43cfc96658aa5f835
tetri.neto <tetri.neto@aliare.co>
2025-11-19 09:31:40 -0300

    Markdown Heading Converter

---
+++ 2025/11/19/script.test.js
@@ -0,0 +1,37 @@
+const convert = require('./script');
+
+//1. convert("# My level 1 heading") should return "<h1>My level 1 heading</h1>".
+//2. convert("My heading") should return "Invalid format".
+//3. convert("##### My level 5 heading") should return "<h5>My level 5 heading</h5>".
+//4. convert("#My heading") should return "Invalid format".
+//5. convert("  ###  My level 3 heading") should return "<h3>My level 3 heading</h3>".
+//6. convert("####### My level 7 heading") should return "Invalid format".
+//7. convert("## My #2 heading") should return "<h2>My #2 heading</h2>".
+
+test("convert 1", () => {
+    expect(convert("# My level 1 heading")).toBe("<h1>My level 1 heading</h1>");
+});
+
+test('convert 2', () => {
+    expect(convert("My heading")).toBe("Invalid format");
+});
+
+test('convert 3', () => {
+    expect(convert("##### My level 5 heading")).toBe("<h5>My level 5 heading</h5>");
+});
+
+test('convert 4', () => {
+    expect(convert("#My heading")).toBe("Invalid format");
+});
+
+test('convert 5', () => {
+    expect(convert("  ###  My level 3 heading")).toBe("<h3>My level 3 heading</h3>");
+});
+
+test('convert 6', () => {
+    expect(convert("####### My level 7 heading")).toBe("Invalid format");
+});
+
+test('convert 7', () => {
+    expect(convert("## My #2 heading")).toBe("<h2>My #2 heading</h2>");
+});
