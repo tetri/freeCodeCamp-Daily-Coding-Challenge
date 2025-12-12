b06e556367ccc4869ebf1b966158a7a7fdee96b0
tetri.neto <tetri.neto@aliare.co>
2025-12-12 17:54:29 -0300

    Remove console.log

--- 2025/11/26/script.js
+++ 2025/11/26/script.js
@@ -2,8 +2,6 @@ function isFizzBuzz(sequence) {
   for (let i = 0; i < sequence.length; i++) {
     let idx = i + 1;
 
-    console.log(idx, sequence[i]);
-
     if (idx % 3 == 0 || idx % 5 == 0) {
       if (idx % 3 == 0 && idx % 5 == 0) {
         if (sequence[i] != "FizzBuzz") return false;
